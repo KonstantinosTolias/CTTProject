173d44c7b82b06cc1d30fb4e414ec10b3ba3ec6f
KonstantinosTolias <77580164+KonstantinosTolias@users.noreply.github.com>
2022-12-09 17:18:45 +0200

    Update ProjectDb.cs

--- CTTProject/Data/ProjectDb.cs
+++ CTTProject/Data/ProjectDb.cs
@@ -1,4 +1,4 @@
-﻿using CTTProject.Models;
+using CTTProject.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CTTProject.Migrations
@@ -13,12 +13,22 @@ namespace CTTProject.Migrations
         public DbSet<Creator> Creators { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<Funds> Funds { get; set; }
-
+        
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Data Source=(local);Initial Catalog=Project_Cloud;Integrated Security = true;TrustServerCertificate=True;
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        
+        
+/* For Azure
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString = "Server=tcp:team-7.database.windows.net,1433;Initial Catalog=Project_Cloud;Persist Security Info=False;User ID=Team7;Password=123456!a;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             optionsBuilder.UseSqlServer(connectionString);
         }
+*/
     }
 
 }
